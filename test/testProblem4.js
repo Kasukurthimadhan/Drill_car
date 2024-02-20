8c4583761bd6cb44aea021ff2159d24415edf3ee
kasukurthi Madhan <kasukurthimadhan@gmail.com>
2024-02-20 13:37:05 +0530

    Testing Problem4.js

---
+++ test/testProblem4.js
@@ -0,0 +1,6 @@
+let inventory=require('../data/data')
+let problem4=require('../problems/problem4')
+
+console.log(problem4(inventory));
+let allCarYears=problem4(inventory)
+module.exports=allCarYears;
