6332109f08cd8501d9a12e7a4a69748185be0e8c
kasukurthi Madhan <kasukurthimadhan@gmail.com>
2024-02-21 14:52:11 +0530

    updated in paths

--- test/testProblem4.js
+++ test/testProblem4.js
@@ -2,5 +2,5 @@ let inventory=require('../data/data')
 let problem4=require('../problems/problem4')
 
 console.log(problem4(inventory));
-let allCarYears=problem4(inventory)
-module.exports=allCarYears;
+// let allCarYears=problem4(inventory)
+module.exports=problem4(inventory)
